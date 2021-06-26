fe99010a96ec386d47660bbd432907268d056881
JF-98 <61771655+JF-98@users.noreply.github.com>
2021-06-26 13:46:41 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 import { run } from "./src/main.js";
 
 const DEFAULT_STEP_SIZE = "15000-20000";
-const DEFAULT_XIAOMI_AMAZFIT_USERNAME = "15523432021";
-const DEFAULT_XIAOMI_AMAZFIT_PASSWORD = "Gq200606";
+const DEFAULT_XIAOMI_AMAZFIT_USERNAME = "15523432021"
+const DEFAULT_XIAOMI_AMAZFIT_PASSWORD = "gq200606"
 
 // 获取环境变量
 const config = {
-  username:  DEFAULT_XIAOMI_AMAZFIT_USERNAME,
-  password:  DEFAULT_XIAOMI_AMAZFIT_PASSWORD ,
-  user_id: process.env.XIAOMI_AMAZFIT_USER_ID,
-  app_token: process.env.XIAOMI_AMAZFIT_APP_TOKEN,
-  step_size: DEFAULT_STEP_SIZE,
+  username: DEFAULT_XIAOMI_AMAZFIT_USERNAME ,
+  password: DEFAULT_XIAOMI_AMAZFIT_PASSWORD ,
+  user_id:  "",
+  app_token: "",
+  step_size:  DEFAULT_STEP_SIZE,
 };
 
 await run(config);
